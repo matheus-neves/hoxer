d7c297af041680aea657494d1de047f4b5d72e28
Matheus Neves <matheusneves1989@gmail.com>
2020-09-29 18:58:56 -0300

    feat: Added server

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import jsonServer from 'json-server';
+import auth from 'json-server-auth';
+
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+app.db = router.db;
+
+app.use(auth);
+app.use(router);
+app.listen(3333);
